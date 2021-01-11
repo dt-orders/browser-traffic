a9955f24c414ad935dffc9d1add23e372e1330a1
robertjahn <rob.jahn@gmail.com>
2021-01-11 09:02:17 -0500

    Add support for username call and update pause to show text in log

--- app.py
+++ app.py
@@ -5,44 +5,33 @@ from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
+
 import argparse
 import time 
 import sys
 import random
 
-def get_geolocation():
-  # https://stackoverflow.com/questions/31755633/fake-geolocation-in-chrome-automation
-  # https://developers.google.com/web/tools/chrome-devtools/device-mode/geolocation
-  location = random.randint(1,5)
-  if location == 1:
-    print("Setting Location to: Frankfurt")
-    latitude = 50.1109
-    longitude = 8.6821
-  elif location == 2:
-    print("Setting Location to: Berlin")
-    latitude = 52.520007
-    longitude = 13.404954
-  elif location == 3:
-    print("Setting Location to: London")
-    latitude = 51.507351
-    longitude = -0.127758
-  elif location == 4:
-    print("Setting Location to: San Fran")
-    latitude = 37.774929
-    longitude = -122.419416
-  else:
-    print("Setting Location to: Mumbai")
-    latitude = 19.075984
-    longitude = 72.877656
+def get_random_username():
+  userarray = ["Allison","Betty","Charlie","David","Eddy","Fransis","Gordon","Helen","Ingrid","Jason"]
+  user = userarray[random.randint(0,9)]
+  print("Setting User to: " + user)
+  return user
 
-  return {
-    "latitude": latitude,
-    "longitude": longitude,
-    "accuracy": 100
-  }
+def get_random_ip():
+  location = random.randint(1,4)
+  if location == 1:
+    print("Setting Location to: Germany/Hamburg/Hamburg")
+    ip = "111.111.111.0" 
+  elif location == 2:
+    print("Setting Location to: US/FLorida/Orlando")
+    ip = "111.111.112.0"
+  else:
+    print("Setting Location to: United Kingdom/City of London/City of London")
+    ip = "111.111.113.0"
+
+  return ip
     
 def get_desired_capabilities():
-
   desired_capabilities = DesiredCapabilities.CHROME.copy()
   desired_capabilities['chrome.page.customHeaders.host'] = 'test.local'
   return desired_capabilities
@@ -57,18 +46,17 @@ def get_chrome_options(show_browser) -> None:
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("window-size=1200x600")
-    chrome_options.add_argument("user-agent=whatever you want")
-    chrome_options.add_argument("custom=rob jahn")
-    #chrome_options.add_argument('--deny-permission-prompts')
-    #chrome_options.add_argument("disable-geolocation")
+    chrome_options.add_argument('--deny-permission-prompts')
+    #chrome_options.add_argument("user-agent=whatever you want")
     
     #prefs = {"profile.default_content_setting_values.geolocation" :2}
     #chrome_options.add_experimental_option("prefs",prefs)
 
     return chrome_options
-
-def pause():
-  time.sleep(2)
+  
+def pause(message="", num_seconds=2):
+  print(message + " sleep: " + str(num_seconds) + " seconds")
+  time.sleep(num_seconds)
 
 if __name__ == "__main__":
 
@@ -98,105 +86,64 @@ if __name__ == "__main__":
     chrome_options = get_chrome_options(args.show)
     desired_capabilities = get_desired_capabilities()
     driver = webdriver.Chrome(options=chrome_options,desired_capabilities=desired_capabilities)  
-
-    #driver.implicitly_wait(10)
+    driver.implicitly_wait(10)
 
     # Basic Flow
     for loop in range(0, num_loops): 
-      #location = get_geolocation()
-      #driver.execute_cdp_cmd("Page.setGeolocationOverride", location)
-      #driver.execute_cdp_cmd("Emulation.setGeolocationOverride", location)
 
-      #driver.get("https://maps.google.com")
-      #wait(driver, 10).until(EC.presence_of_element_located((By.ID, "widget-mylocation"))).click()
-      #pause()
+      ip = get_random_ip()
+      header = { "x-dt-orders": ip }
+      print("Setting x-dt-orders ip to: " + str(header))
+      driver.execute_cdp_cmd("Network.enable", {})
+      driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": header})
 
-      print("Loop " + str(loop + 1) + " of " + str(num_loops) + "    Running with base URL: " + url)
+      userurl =  url + "?username=" + username
+      print("Loop " + str(loop + 1) + " of " + str(num_loops) + "    Running with base URL: " + userurl)
       print("..Opening Home Page")
-      driver.get(url)
+      username = get_random_username()
+      driver.get(userurl)
       assert "Dynatrace Order Processing" in driver.title
-      #pause()
-      pause()
-      pause()
-      pause()
-      pause()
-      #print(driver.page_source)
+      pause("Home")
 
       print("..Customer Flow")
       wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Customer"))).click()
-      pause()
+      pause("Customer List")
       wait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[2]/table/tbody/tr[1]/td[1]/a"))).click()
-      pause()
-      wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Home"))).click()
-      pause()
-      break
-      """
-      driver.find_element_by_link_text("Customer").click()
-      pause()
-      # first Link to detail record in the table
-      driver.find_element_by_xpath("/html/body/div/div[2]/table/tbody/tr[1]/td[1]/a").click()
-      pause()
-      driver.find_element_by_link_text("Home").click()
-      pause()
-      """
+      pause("First Customer")
+      driver.get(url + "?username=" + username)
+      #wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Home"))).click()
+      pause("Home")
 
       print("..Catalog Flow")
       wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Catalog"))).click()
-      pause()
+      pause("Catalog List")
       wait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[2]/table/tbody/tr[1]/td[1]/a"))).click()
-      pause()
-      wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Home"))).click()
-      pause()
-      """
-      driver.find_element_by_link_text("Catalog").click()
-      pause()
-      # first Link to detail record in the table
-      driver.find_element_by_xpath("/html/body/div/div[2]/table/tbody/tr[1]/td[1]/a").click()
-      pause()
-      driver.find_element_by_link_text("Home").click()
-      pause()
-      """
+      pause("First Catalog Item")
+      driver.get(url + "?username=" + username)
+      #wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Home"))).click()
+      pause("Home")
 
       print("..Search Item Flow")
       wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Search Items"))).click()
-      pause()
+      pause("Search Item Form")
       element = wait(driver, 10).until(EC.presence_of_element_located((By.NAME, "query")))
       element.send_keys("iPod nano")
       element.send_keys(Keys.RETURN)
-      pause()
-      wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Home"))).click()
-      pause()
-      """
-      driver.find_element_by_link_text("Search Items").click()
-      pause()
-      elem = driver.find_element_by_name("query")
-      elem.clear()
-      elem.send_keys("iPod nano")
-      elem.send_keys(Keys.RETURN)
-      pause()
-      driver.find_element_by_link_text("Home").click()
-      pause()
-      """
+      pause("Search Item")
+      driver.get(url + "?username=" + username)
+      #wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Home"))).click()
+      pause("Home")
 
       print("..Order Flow")
       wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Order"))).click()
-      pause()
+      pause("Order Home Page")
       wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Add Order"))).click()
-      pause()
+      pause("Add Order Form")
       wait(driver, 10).until(EC.presence_of_element_located((By.NAME, "addLine"))).click()
-      pause()
-      wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Home"))).click()
-      pause()
-      """
-      driver.find_element_by_link_text("Order").click()
-      pause()
-      driver.find_element_by_link_text("Add Order").click()
-      pause()
-      driver.find_element_by_name("addLine").click()    
-      pause()
-      driver.find_element_by_link_text("Home").click()
-      pause()
-      """
+      pause("Add Order Line Item")
+      driver.get(url + "?username=" + username)
+      #wait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Home"))).click()
+      pause("Home")
 
     # end of loop
-    #driver.close()
+    driver.close()
